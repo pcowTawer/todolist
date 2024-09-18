a26fd3b10e6dc66bd2c2e08ce92ec5d29351e3b6
Ilya_Prostakov <prostocowtawer@gmail.com>
2024-09-18 16:41:28 +0700

    Feature(): Add reactive form taskForm in todo-list-component

--- client-web/src/app/todo-list/todo-list.component.ts
+++ client-web/src/app/todo-list/todo-list.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { TodoItemComponent } from "../todo-item/todo-item.component";
 import { Task } from '../task';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [TodoItemComponent],
+  imports: [TodoItemComponent, ReactiveFormsModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
+  taskForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+  });
+
+  handleSubmit() {
+    this.tasks.push({
+      'id': (Math.random() + 1).toString(36).substring(7),
+      'title': this.taskForm.value.title ?? '',
+      'description': this.taskForm.value.description ?? '',
+      'completed': false
+    } as Task)
+  }
+  
   tasks: Task[] = [
     {
       'id': '1',
